6ce68601ed3488b2a9a50f5d06f3410cd43069df
Danijal Alibegović <dalibegovi2@etf.unsa.ba>
2025-05-28 11:41:49 +0200

    lijepo

---
+++ ooadepazar/ooadepazar/Controllers/ArtikalController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ooadepazar.Data;
+using ooadepazar.Models;
+
+namespace ooadepazar.Controllers
+{
+    public class ArtikalController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArtikalController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Artikal
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Artikal.ToListAsync());
+        }
+
+        // GET: Artikal/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artikal = await _context.Artikal
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (artikal == null)
+            {
+                return NotFound();
+            }
+
+            return View(artikal);
+        }
+
+        // GET: Artikal/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artikal/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Naziv,Stanje,Opis,Cijena,Lokacija,DatumObjave")] Artikal artikal)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(artikal);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artikal);
+        }
+
+        // GET: Artikal/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artikal = await _context.Artikal.FindAsync(id);
+            if (artikal == null)
+            {
+                return NotFound();
+            }
+            return View(artikal);
+        }
+
+        // POST: Artikal/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Naziv,Stanje,Opis,Cijena,Lokacija,DatumObjave")] Artikal artikal)
+        {
+            if (id != artikal.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(artikal);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArtikalExists(artikal.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(artikal);
+        }
+
+        // GET: Artikal/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var artikal = await _context.Artikal
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (artikal == null)
+            {
+                return NotFound();
+            }
+
+            return View(artikal);
+        }
+
+        // POST: Artikal/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var artikal = await _context.Artikal.FindAsync(id);
+            if (artikal != null)
+            {
+                _context.Artikal.Remove(artikal);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArtikalExists(int id)
+        {
+            return _context.Artikal.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ooadepazar/ooadepazar/Controllers/NarudzbaController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ooadepazar.Data;
+using ooadepazar.Models;
+
+namespace ooadepazar.Controllers
+{
+    public class NarudzbaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NarudzbaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Narudzba
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Narudzba.Include(n => n.Artikal);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Narudzba/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var narudzba = await _context.Narudzba
+                .Include(n => n.Artikal)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (narudzba == null)
+            {
+                return NotFound();
+            }
+
+            return View(narudzba);
+        }
+
+        // GET: Narudzba/Create
+        public IActionResult Create()
+        {
+            ViewData["ArtikalID"] = new SelectList(_context.Artikal, "ID", "Lokacija");
+            return View();
+        }
+
+        // POST: Narudzba/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,DatumNarudzbe,DatumObrade,Status,ArtikalID")] Narudzba narudzba)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(narudzba);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtikalID"] = new SelectList(_context.Artikal, "ID", "Lokacija", narudzba.ArtikalID);
+            return View(narudzba);
+        }
+
+        // GET: Narudzba/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var narudzba = await _context.Narudzba.FindAsync(id);
+            if (narudzba == null)
+            {
+                return NotFound();
+            }
+            ViewData["ArtikalID"] = new SelectList(_context.Artikal, "ID", "Lokacija", narudzba.ArtikalID);
+            return View(narudzba);
+        }
+
+        // POST: Narudzba/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,DatumNarudzbe,DatumObrade,Status,ArtikalID")] Narudzba narudzba)
+        {
+            if (id != narudzba.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(narudzba);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NarudzbaExists(narudzba.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ArtikalID"] = new SelectList(_context.Artikal, "ID", "Lokacija", narudzba.ArtikalID);
+            return View(narudzba);
+        }
+
+        // GET: Narudzba/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var narudzba = await _context.Narudzba
+                .Include(n => n.Artikal)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (narudzba == null)
+            {
+                return NotFound();
+            }
+
+            return View(narudzba);
+        }
+
+        // POST: Narudzba/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var narudzba = await _context.Narudzba.FindAsync(id);
+            if (narudzba != null)
+            {
+                _context.Narudzba.Remove(narudzba);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NarudzbaExists(int id)
+        {
+            return _context.Narudzba.Any(e => e.ID == id);
+        }
+    }
+}
