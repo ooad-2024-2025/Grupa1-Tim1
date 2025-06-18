bb318ebf83a3ee55889696ccd7719dd8cddbc942
Bakir Cinjarevic <bakir.cinjarevic@gmail.com>
2025-06-18 15:59:02 +0200

    404 page

--- ePazar/ooadepazar/Program.cs
+++ ePazar/ooadepazar/Program.cs
@@ -34,6 +34,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // Make sure to include this for serving static files
 
